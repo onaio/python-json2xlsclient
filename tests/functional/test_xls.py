414829d18396406e77836873806f635b83b04cc6
James Nzomo <kazikubwa@gmail.com>
2014-10-01 12:30:13 +0300

    Work in Progress

---
+++ tests/functional/test_xls.py
@@ -0,0 +1,19 @@
+import testtools
+
+
+class TestFunctionalXLS(testtools.TestCase):
+
+    def test_generate_xls(self):
+        pass
+
+    def test_get_generated_xls(self):
+        pass
+
+    def test_regenerate_xls(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
