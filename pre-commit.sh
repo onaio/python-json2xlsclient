da20a3756015117e5f3ca4a80a73a690e5ca8bf7
James Nzomo <kazikubwa@gmail.com>
2014-10-07 15:36:25 +0300

    JZ: Add coverage for tests

--- pre-commit.sh
+++ pre-commit.sh
@@ -7,7 +7,7 @@ git diff --cached --name-only | \
     grep -E $FILES_PATTERN | \
     GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "$FORBIDDEN" references. Please remove them before committing' && exit 1
 
-nosetests tests --with-doctest
+nosetests tests --with-doctest --with-coverage
 RESULT=$?
 git stash pop -q
 [ $RESULT -ne 0 ] && exit 1
