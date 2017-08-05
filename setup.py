c183d2ad35e92115f417e814a542e4596d88f71b
Ukang'a Dickson <ukanga@gmail.com>
2017-08-05 10:22:47 +0300

    specify packages in setup
    Installing with pip-sync ensures the json2xlsclient package is installed.

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from setuptools import setup
 setup(
     name="python-json2xlsclient",
     description=("json-to-xls client."),
+    packages=["json2xlsclient"],
     install_requires=["requests"]
 )
